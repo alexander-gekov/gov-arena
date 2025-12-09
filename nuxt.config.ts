9a92c388ff545c0006e864659271c8e4b9e92894
Aleksandar Gekov <alexander.gekov00@gmail.com>
2025-12-10 00:17:23 +0200

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,36 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: ['shadcn-nuxt', '@vueuse/nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/components/ui'
+  },
+  plugins: [
+    '~/plugins/ssr-width.ts',
+  ],
+  runtimeConfig: {
+    aiGatewayApiKey: process.env.AI_GATEWAY_API_KEY,
+  },
+})
